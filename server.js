cefd64c83e21f3d1da7c6a49ef36f06ce2e5efef
PhrogLover <dan.h.champion@durham.ac.uk>
2021-01-03 15:38:17 +0000

    Server JS added

---
+++ server.js
@@ -0,0 +1,9 @@
+cons express = require("express");
+const path = require("path");
+
+const app = express();
+app.get("/*", (req,res) => {
+    res.sendFile(path.resolve("index.html"));
+});
+
+app.listen(process.env.PORT || 5060, () => console.log("Server Running"));
